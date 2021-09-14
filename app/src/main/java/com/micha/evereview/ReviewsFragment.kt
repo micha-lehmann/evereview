c8f453b5332f6720a56688f855fd80ee0ff63315
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-09-14 09:50:11 +0200

    Add empty Reviews Fragment

---
+++ app/src/main/java/com/micha/evereview/ReviewsFragment.kt
@@ -0,0 +1,31 @@
+package com.micha.evereview
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+
+class ReviewsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ReviewsFragment()
+    }
+
+    private val viewModel by activityViewModels<ReviewsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.reviews_fragment, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/com/micha/evereview/ReviewsViewModel.kt
@@ -0,0 +1,7 @@
+package com.micha.evereview
+
+import androidx.lifecycle.ViewModel
+
+class ReviewsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
