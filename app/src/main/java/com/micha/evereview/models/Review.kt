c95bb30bd9f8635b702d3f1f1615c365c97002d5
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-09-20 09:52:18 +0200

    Remove Review timestamp
    I didn't use it so far and it would have made things more complicated later on.

--- app/src/main/java/com/micha/evereview/models/Review.kt
+++ app/src/main/java/com/micha/evereview/models/Review.kt
@@ -1,11 +1,9 @@
 package com.micha.evereview.models
 
 import java.io.Serializable
-import java.util.*
 
 data class Review<T : ReviewItem>(
     val item: T,
     val rating: Double,
-    val timestamp: Date,
     val note: String?
 ) : Serializable
--- app/src/main/java/com/micha/evereview/reviews/ReviewsRepository.kt
+++ app/src/main/java/com/micha/evereview/reviews/ReviewsRepository.kt
@@ -1,7 +1,6 @@
 package com.micha.evereview.reviews
 
 import com.micha.evereview.models.*
-import java.util.*
 
 class ReviewsRepository {
     fun getReviews(onSuccess: (reviews: List<Review<out ReviewItem>>) -> Unit) {
@@ -11,31 +10,26 @@ class ReviewsRepository {
             Review(
                 Movie("Avengers", 143, 2012),
                 7.5,
-                Date(),
                 "Good film."
             ),
             Review(
                 Movie("The Incredible Hulk", 112, 2008),
                 3.0,
-                Date(),
                 null
             ),
             Review(
                 Series("Agents of S.H.I.E.L.D.", 1, 22),
                 8.5,
-                Date(),
                 "Favorite character: Agent Coulson!"
             ),
             Review(
                 Music("Tendon", "Igorrr", "Crazy Shit"),
                 7.5,
-                Date(),
                 "Completely crazy but actually pretty good. Not easy to listen to though."
             ),
             Review(
                 Book("I don't know any books", "Goethe?", "Drama"),
                 0.0,
-                Date(),
                 "Not a book person."
             )
         ))
