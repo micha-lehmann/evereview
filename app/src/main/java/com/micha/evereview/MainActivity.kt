7911e07dffd9cc087499d15f804463ef3f6cd26b
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-09-20 16:11:40 +0200

    Remove unused vars from MainActivity

--- app/src/main/java/com/micha/evereview/MainActivity.kt
+++ app/src/main/java/com/micha/evereview/MainActivity.kt
@@ -2,11 +2,9 @@ package com.micha.evereview
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.micha.evereview.databinding.ActivityMainBinding
 import com.micha.evereview.edit.EditReviewActivity
-import com.micha.evereview.reviews.ReviewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,8 +13,6 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private val model by viewModels<ReviewsViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.root)
@@ -24,7 +20,5 @@ class MainActivity : AppCompatActivity() {
         layout.add.setOnClickListener {
             startActivity(Intent(this, EditReviewActivity::class.java))
         }
-
-        val mainMenu = layout.mainMenu
     }
 }
