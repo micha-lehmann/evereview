9435231da2b956a70de6daae2729a94a42d4c676
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-09-20 10:29:39 +0200

    Give Review an ID to better handle it with fragments.

--- app/src/main/java/com/micha/evereview/models/Review.kt
+++ app/src/main/java/com/micha/evereview/models/Review.kt
@@ -5,5 +5,6 @@ import java.io.Serializable
 data class Review<T : ReviewItem>(
     val item: T,
     val rating: Double,
-    val note: String?
+    val note: String?,
+    val id: Int
 ) : Serializable
--- app/src/main/java/com/micha/evereview/reviews/ReviewsRepository.kt
+++ app/src/main/java/com/micha/evereview/reviews/ReviewsRepository.kt
@@ -10,27 +10,32 @@ class ReviewsRepository {
             Review(
                 Movie("Avengers", 143, 2012),
                 7.5,
-                "Good film."
+                "Good film.",
+                1
             ),
             Review(
                 Movie("The Incredible Hulk", 112, 2008),
                 3.0,
-                null
+                null,
+                2
             ),
             Review(
                 Series("Agents of S.H.I.E.L.D.", 1, 22),
                 8.5,
-                "Favorite character: Agent Coulson!"
+                "Favorite character: Agent Coulson!",
+                3
             ),
             Review(
                 Music("Tendon", "Igorrr", "Crazy Shit"),
                 7.5,
-                "Completely crazy but actually pretty good. Not easy to listen to though."
+                "Completely crazy but actually pretty good. Not easy to listen to though.",
+                4
             ),
             Review(
                 Book("I don't know any books", "Goethe?", "Drama"),
                 0.0,
-                "Not a book person."
+                "Not a book person.",
+                5
             )
         ))
     }
