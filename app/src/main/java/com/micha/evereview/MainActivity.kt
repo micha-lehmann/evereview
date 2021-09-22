072a04a1801a2adaf3e680301344338f4256588f
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-09-22 10:18:19 +0200

    Restructure main activity to not use a fragment
    It was hurting me more than helping.
    
    Also remove BottomNavigation, it wasn't functional and useless.

--- app/src/main/java/com/micha/evereview/MainActivity.kt
+++ app/src/main/java/com/micha/evereview/MainActivity.kt
@@ -2,9 +2,13 @@ package com.micha.evereview
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.micha.evereview.databinding.ActivityMainBinding
 import com.micha.evereview.edit.EditReviewActivity
+import com.micha.evereview.reviews.ReviewsAdapter
+import com.micha.evereview.reviews.ReviewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val model by viewModels<ReviewsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.root)
@@ -20,5 +26,11 @@ class MainActivity : AppCompatActivity() {
         layout.add.setOnClickListener {
             startActivity(Intent(this, EditReviewActivity::class.java))
         }
+
+        layout.reviews.layoutManager = LinearLayoutManager(this)
+        model.reviews.observe(this) { reviews ->
+            layout.reviews.adapter = ReviewsAdapter(reviews)
+        }
+        model.loadReviews()
     }
 }
